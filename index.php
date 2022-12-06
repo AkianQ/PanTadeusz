89b9d05baa10dcf5de45051165bda4ec5ad5fc58
AkianQ <s14194@PK-IE-Serwer.PPUZ>
2022-12-06 10:22:20 +0100

    index.html changed into index.php

--- index.php
+++ index.php
@@ -55,30 +55,31 @@
     <div >
         <h2>Spis treści</h2>
         <ul>
-            <li><a class="text-decoration-none text-dark" href="./k1.html">Księga 1</a></li>
-            <li><a class="text-decoration-none text-dark" href="./k2.html">Księga 2</a></li>
-            <li><a class="text-decoration-none text-dark" href="./k3.html">Księga 3</a></li>
-            <li><a class="text-decoration-none text-dark" href="./k4.html">Księga 4</a></li>
-            <li><a class="text-decoration-none text-dark" href="./k5.html">Księga 5</a></li>
-            <li><a class="text-decoration-none text-dark" href="./k6.html">Księga 6</a></li>
-            <li><a class="text-decoration-none text-dark" href="./k7.html">Księga 7</a></li>
-            <li><a class="text-decoration-none text-dark" href="./k8.html">Księga 8</a></li>
-            <li><a class="text-decoration-none text-dark" href="./k9.html">Księga 9</a></li>
-            <li><a class="text-decoration-none text-dark" href="./k10.html">Księga 10</a></li>
-            <li><a class="text-decoration-none text-dark" href="./k11.html">Księga 11</a></li>
-            <li><a class="text-decoration-none text-dark" href="./k12.html">Księga 12</a></li>
+        <li><a style="text-decoration: none; color: white;" href="./index.php">Strona Główna</li>
+        <?php
+            for($k=1; $k<=12; $k++)
+              {
+                print("<li><a class='text-decoration-none text-light' href='./index.php?k=$k&name=ind'>Księga $k</a></li>");
+              }
+              ?>
         </ul>
     </div>
     </div>
     <div class="col-8">
-        <img src="./pan tadeusz.png" alt="Pan Tadeusz :)">
     </div>
         </div>
             </div>
     <div>
-        <?php 
-            include_once("./k1.html")
-        ?>
+        <center><?php 
+            if(isset($_GET['k']))
+            {
+            $k = $_GET['k'];
+            include_once("./k$k.html");
+            }else
+            {
+              print('<img src="./pan tadeusz.jfif" alt="Pan Tadeusz :)">');
+            }
+        ?></center>
     </div>
     <footer class="text-center bg-warning">
         <p>&copy Jakub Bąbol &copy</p>
